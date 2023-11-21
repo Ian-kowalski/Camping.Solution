0754016bb1d92706318ad41f415a12d68fd0b903
Hilmi <hilmielhajji@gmail.com>
2023-11-21 11:59:23 +0100

    campSite data ophalen

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,9 @@
+﻿using camping.Database;
+
+SiteData siteData = new();
+
+
+foreach (var item in siteData.GetSites("campSiteID"))
+{
+    Console.WriteLine(item);
+}
---
+++ camping.Database/SiteData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Microsoft;
+using Microsoft.Data.SqlClient;
+
+namespace camping.Database
+{
+    public class SiteData
+    {
+        private string connectionString = "Data Source=127.0.0.1;Initial Catalog=Camping;Persist Security Info=True;User ID=sa;Password=r2Njj8#4;Trust Server Certificate=True";
+    
+
+        public List<string> GetSites(string sitedetail)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(); builder.DataSource = "127.0.0.1"; builder.UserID = "sa"; builder.Password = "r2Njj8#4"; builder.InitialCatalog = "TestDB"; builder.TrustServerCertificate = false;
+            string sql = $"SELECT {sitedetail} FROM campSite";
+
+            using(var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlDataReader reader;
+                List<string> result = new List<string>();
+
+                using(var command = new SqlCommand(sql, connection))
+                {
+                    reader = command.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        result.Add(Convert.ToString(reader.GetInt32(0)));
+                    }
+                }
+                connection.Close();
+                return result;
+            }
+        }
+    }
+}
