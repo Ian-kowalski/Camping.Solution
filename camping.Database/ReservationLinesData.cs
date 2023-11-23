ad2d6e3d200a5ea25b411c3073ad8beab32d86b1
Hilmi <hilmielhajji@gmail.com>
2023-11-23 09:31:42 +0100

    reservationlines data

--- camping.Database/ReservationLinesData.cs
+++ camping.Database/ReservationLinesData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace camping.Database
 {
-    internal class ReservationLinesData
+    public class ReservationLinesData
     {
+        private string connectionString = "Data Source=127.0.0.1;Initial Catalog=Camping;Persist Security Info=True;User ID=sa;Password=r2Njj8#4;Trust Server Certificate=True";
+
+
+        public int GetCampSiteID(int reservationID)
+        {
+            string sql = $"SELECT campSiteID FROM reservationLines WHERE reservationID = {reservationID}";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlDataReader reader;
+                int result;
+
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    reader = command.ExecuteReader();
+
+                    result = reader.GetInt32(0);
+                }
+                connection.Close();
+                return result;
+            }
+        }
     }
 }
