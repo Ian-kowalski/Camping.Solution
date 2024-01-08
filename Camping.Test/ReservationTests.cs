ab256a94b6ee7362ff279f579222660bf6356875
Ian-kowalski <ian.kowalski@hotmail.com>
2024-01-08 19:50:33 +0100

    unittesten nu werkent zonder errors

--- Camping.Test/ReservationTests.cs
+++ Camping.Test/ReservationTests.cs
@@ -1,69 +1,81 @@
-﻿using camping.Database;
-using Newtonsoft.Json.Bson;
+﻿using camping.Core;
+using camping.Database;
+using System.Linq;
 
-namespace Camping.Test
+
+namespace camping.Test
 {
+    [TestFixture]
     public class ReservationTests
     {
 
-        SshConnection sshConnection;
+        private SshConnection sshConnection;
+        private RetrieveData retrieveData;
+        private Reservation TestReservation;
+        private Reservation TestAddReservation;
+
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
             sshConnection = new SshConnection();
+
+            SiteData siteData = new();
+            ReservationRepository reservationData = new();
+            retrieveData = new(siteData, reservationData);
         }
 
         [SetUp]
         public void Setup()
         {
-
-        }
-
-        public void DeleteReservation(int reservationID)
-        {
-
+            retrieveData.addReservation(1,DateTime.Today.ToString("MM-dd-yyyy"),DateTime.Today.ToString("MM-dd-yyyy"),"Test","Unit","Setup","database","camping","1234XX","1a",06123456);
+            TestReservation = retrieveData.Reservations[retrieveData.Reservations.Count - 1];
         }
 
 
-        public void addReservationLine(int campSiteID, int reservationID)
+        [Test]
+        public void DeleteReservation()
         {
-
+            Assert.IsTrue(retrieveData.DeleteReservation(TestReservation.ReservationID) );
         }
 
-
-        public void addReservation(int campSiteID, string startDate, string endDate, string firstName, string preposition, string lastName, string adress, string city, string postalcode, string houseNumber, int phoneNumber)
+        [Test]
+        public void addReservation()
         {
-
+            Assert.IsTrue(retrieveData.addReservation(2, DateTime.Today.ToString("MM-dd-yyyy"), DateTime.Today.ToString("MM-dd-yyyy"), "addReservationLine", "Unit", "Setup", "database", "camping", "1234XX", "1a", 06123456));
+            TestAddReservation = retrieveData.Reservations[retrieveData.Reservations.Count - 1];
         }
 
-        public void getReservationID(int visitorID, string startDate, string endDate)
+        [Test]
+        public void GetOtherAvailableReservation()
         {
-
+            Assert.IsTrue(retrieveData.GetOtherAvailableReservations(TestReservation.SiteID, TestReservation.StartDate.ToShortDateString(), TestReservation.EndDate.ToShortDateString(), TestReservation.ReservationID));
         }
 
-        public void GetOtherAvailableReservation(int campSite, string startDate, string endDate, int reservationID)
+        [Test]
+        public void UpdateReservation()
         {
-
+            Visitor visitor = new Visitor(TestReservation.Visitor.VisitorID, TestReservation.Visitor.FirstName, TestReservation.Visitor.LastName, "Update", TestReservation.Visitor.Adress, TestReservation.Visitor.City, TestReservation.Visitor.PostalCode, TestReservation.Visitor.HouseNumber, TestReservation.Visitor.PhoneNumber);
+            Assert.IsTrue(retrieveData.UpdateReservation(TestReservation.ReservationID, TestReservation.StartDate, visitor, TestReservation.EndDate, TestReservation.SiteID));
         }
 
-        public void UpdateReservationLines(int campSiteID, int reservationID)
+        [Test]
+        public void HasUpcomingReservations()
         {
-
-        }
-
-        public void UpdateReservation(int reservationID, DateTime startDate, DateTime endDate, int campSiteID)
-        {
-
-        }
-
-        public void HasUpcomingReservations(int campSiteID, DateTime date)
-        {
-
+            Assert.IsTrue(retrieveData.HasUpcomingReservations(TestReservation.SiteID));
         }
 
         [TearDown]
-        public void TearDown() 
+        public void TearDown()
         {
+            if(retrieveData.Reservations.Contains(TestReservation) )
+            {
+                retrieveData.DeleteReservation(TestReservation.ReservationID);
+            }
+
+            if (retrieveData.Reservations.Contains(TestAddReservation))
+            {
+                retrieveData.DeleteReservation(TestAddReservation.ReservationID);
+            }
 
         }
 
